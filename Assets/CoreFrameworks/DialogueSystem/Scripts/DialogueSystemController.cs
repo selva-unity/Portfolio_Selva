06692dd8e7a791348605400f51d31a927af5fa54
selvaganeshNTCPWC <selva.onedigit@gmail.com>
2026-02-06 17:07:35 +0530

    Updates

--- Assets/CoreFrameworks/DialogueSystem/Scripts/DialogueSystemController.cs
+++ Assets/CoreFrameworks/DialogueSystem/Scripts/DialogueSystemController.cs
@@ -7,8 +7,9 @@ public class DialogueSystemController : MonoBehaviour
     void Start()
     {
         DialogueManager.instance.ShowDialogue(DialogueManager.instance.narrationSO, SceneNarration.Inroduction, DialogueType.NarrationType, false, () => {
-            Debug.Log("Dialogue Finished");
+            Debug.Log("Dialogue Finished inside");
         });
+        Debug.Log("Dialogue Finished outside");
         // DialogueManager.instance.ShowDialogue(DialogueManager.instance.convoSO, SceneNarration.ResponseConversation, DialogueType.ConversationType, false, () => {
         //     Debug.Log("Dialogue Finished");
         // });
